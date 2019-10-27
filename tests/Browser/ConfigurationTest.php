46b311dc6990fa25f25b72872d22718fdefa9663
MilesChou <jangconan@gmail.com>
2019-10-27 11:43:56 +0800

    Add test comments

--- tests/Browser/ConfigurationTest.php
+++ tests/Browser/ConfigurationTest.php
@@ -8,12 +8,18 @@ use Tests\DuskTestCase;
 class ConfigurationTest extends DuskTestCase
 {
     /**
+     * Retrieve and use the OpenID Provider Configuration Information.
+     *
+     * Read and use the JSON object returned from the OpenID Connect Provider.
+     *
      * @see DiscoveryOpenIDConfiguration
      * @link https://rp.certification.openid.net:8080/log/oidcphp-rp.configuration/rp-discovery-openid-configuration.txt
+     * @link https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
      */
     public function testDiscoveryOpenIDConfiguration(): void
     {
         $this->browse(function (Browser $browser) {
+            // http://localhost:8000/rp/configuration/rp-discovery-openid-configuration
             $browser->visit('/rp/configuration/rp-discovery-openid-configuration')
                 ->assertSee('authorization_endpoint')
                 ->assertSee('id_token_signing_alg_values_supported')
--- tests/Browser/RpTest.php
+++ tests/Browser/RpTest.php
@@ -9,12 +9,17 @@ use Tests\DuskTestCase;
 class RpTest extends DuskTestCase
 {
     /**
+     * Make an authentication request using the Authorization Code Flow.
+     *
+     * An authentication response containing an authorization code.
+     *
      * @see ResponseTypeCode
      * @link https://rp.certification.openid.net:8080/log/oidcphp-rp.code/rp-response_type-code.txt
      */
     public function testRpResponseTypeCode(): void
     {
         $this->browse(function (Browser $browser) {
+            // http://localhost:8000/rp/code/rp-response_type-code
             $browser->visit('/rp/code/rp-response_type-code')
                 ->waitForText('1b2fc9341a16ae4e30082965d537ae47c21a0f27fd43eab78330ed81751ae6db', 10)
                 ->assertQueryStringHas('code');
@@ -22,12 +27,18 @@ class RpTest extends DuskTestCase
     }
 
     /**
+     * Request claims using scope values.
+     *
+     * A UserInfo Response containing the requested claims. If no access token is issued (when using Implicit Flow with response_type='id_token') the ID Token contains the requested claims.
+     *
      * @see ScopeUserinfoClaims
      * @link https://rp.certification.openid.net:8080/log/oidcphp-rp.code/rp-scope-userinfo-claims.txt
+     * @link https://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims
      */
     public function testRpScopeUserinfoClaims(): void
     {
         $this->browse(function (Browser $browser) {
+            // http://localhost:8000/rp/code/rp-scope-userinfo-claims
             $browser->visit('/rp/code/rp-scope-userinfo-claims')
                 ->waitForText('1b2fc9341a16ae4e30082965d537ae47c21a0f27fd43eab78330ed81751ae6db', 10)
                 ->assertSee('name')
@@ -38,24 +49,36 @@ class RpTest extends DuskTestCase
     }
 
     /**
+     * Pass a 'nonce' value in the Authentication Request. Verify the 'nonce' value returned in the ID Token.
+     *
+     * Identify that the 'nonce' value in the ID Token is invalid and reject the ID Token.
+     *
      * @see NonceInvalid
      * @link https://rp.certification.openid.net:8080/log/oidcphp-rp.code/rp-nonce-invalid.txt
+     *
+     * @link https://openid.net/specs/openid-connect-core-1_0.html#NonceNotes
      */
     public function testRpNonceInvalid(): void
     {
         $this->browse(function (Browser $browser) {
+            // http://localhost:8000/rp/code/rp-nonce-invalid
             $browser->visit('/rp/code/rp-nonce-invalid')
                 ->assertSee('Receive an invalid ID token');
         });
     }
 
     /**
+     * Use the 'client_secret_basic' method to authenticate at the Authorization Server when using the token endpoint.
+     *
+     * A Token Response, containing an ID token.
+     *
      * @see TokenEndpointClientSecretBasic
      * @link https://rp.certification.openid.net:8080/log/oidcphp-rp.code/rp-token_endpoint-client_secret_basic.txt
      */
     public function testRpTokenEndpointClientSecretBasic(): void
     {
         $this->browse(function (Browser $browser) {
+            // http://localhost:8000/rp/code/rp-token_endpoint-client_secret_basic
             $browser->visit('/rp/code/rp-token_endpoint-client_secret_basic')
                 ->assertSee('id_token');
         });
