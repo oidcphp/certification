d684b85231a1850d676da30753a129c357a41af1
MilesChou <jangconan@gmail.com>
2019-10-27 11:14:06 +0800

    Add Laravel Dusk test on Travis CI

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -31,6 +31,7 @@ abstract class DuskTestCase extends BaseTestCase
     {
         $options = (new ChromeOptions)->addArguments([
             '--disable-gpu',
+            '--headless',
             '--window-size=1920,1080',
         ]);
 
